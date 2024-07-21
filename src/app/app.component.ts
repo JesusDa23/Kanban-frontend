455c98d63ae1fbe7b352d4d50c8f458316b807a1
Jesús David Chavarro Castro <jesusdavidchavarro23@gmail.com>
2024-07-20 21:15:55 -0500

    add the header with your styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { BoardsComponent } from "./pages/boards/boards.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, BoardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { BoardsComponent } from './pages/boards/boards.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '', component: BoardsComponent
+}];
